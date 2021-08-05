c6395b34ac99bc6545c75960b41180cc1fe807a9
hayesall <alexander@batflyer.net>
2021-08-05 14:27:05 -0400

    ✨ Add parser support for regression/classification

--- cmd/parser/ilplang_base_listener.go
+++ cmd/parser/ilplang_base_listener.go
@@ -27,8 +27,26 @@ func (s *BaseILPLangListener) EnterStart(ctx *StartContext) {}
 // ExitStart is called when production start is exited.
 func (s *BaseILPLangListener) ExitStart(ctx *StartContext) {}
 
+// EnterClassification_task is called when production classification_task is entered.
+func (s *BaseILPLangListener) EnterClassification_task(ctx *Classification_taskContext) {}
+
+// ExitClassification_task is called when production classification_task is exited.
+func (s *BaseILPLangListener) ExitClassification_task(ctx *Classification_taskContext) {}
+
+// EnterRegression_task is called when production regression_task is entered.
+func (s *BaseILPLangListener) EnterRegression_task(ctx *Regression_taskContext) {}
+
+// ExitRegression_task is called when production regression_task is exited.
+func (s *BaseILPLangListener) ExitRegression_task(ctx *Regression_taskContext) {}
+
 // EnterTerm is called when production term is entered.
 func (s *BaseILPLangListener) EnterTerm(ctx *TermContext) {}
 
 // ExitTerm is called when production term is exited.
 func (s *BaseILPLangListener) ExitTerm(ctx *TermContext) {}
+
+// EnterRegression_term is called when production regression_term is entered.
+func (s *BaseILPLangListener) EnterRegression_term(ctx *Regression_termContext) {}
+
+// ExitRegression_term is called when production regression_term is exited.
+func (s *BaseILPLangListener) ExitRegression_term(ctx *Regression_termContext) {}
